711071fd1fa2b4e1e8c39729b5a52abeffb14c1e
Martijn van Zuilen <martijn@titan.localdomain>
2019-12-10 22:45:16 +0100

    modified:   config.h 	deleted:    drw.o 	deleted:    dwm 	deleted:    dwm-activetagindicatorbar-6.2.diff 	deleted:    dwm-autostart-20161205-bb3bd6f.diff 	deleted:    dwm.o 	deleted:    systray.patch 	deleted:    util.o

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=13" };
+static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "font-awesome:size=10";
 static const char col_gray1[]       = "#20201d";
 static const char col_gray2[]       = "#6684e1";
