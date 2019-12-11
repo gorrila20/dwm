fd43cb1140d04ec8e2b735e1c5d2ed8c6173979e
Martijn van Zuilen <martijn@voyager.localdomain>
2019-12-11 08:35:15 +0100

    modified:   config.def.h 	new file:   config.def.h.orig 	modified:   config.h 	modified:   dwm.c 	modified:   dwm.c.orig 	deleted:    drw.o 	deleted:    dwm 	deleted:    dwm-activetagindicatorbar-6.2.diff 	deleted:    dwm-autostart-20161205-bb3bd6f.diff 	deleted:    dwm.o 	deleted:    systray.patch 	deleted:    util.o

--- config.h
+++ config.h
@@ -58,6 +58,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *browser[]  = { "qutebrowser", NULL};
+static const char *volumecontrol[] = { "pavucontrol", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -76,6 +78,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,			XK_q,	   spawn,	   {.v = browser} },
+	{ MODKEY,	                XK_v,	   spawn,	   {.v = volumecontrol} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
